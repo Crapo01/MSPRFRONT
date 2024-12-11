7bb5b8dc7cceeaad0ab81d17eacf976c415bd7a0
Crapo01 <capusluc@gmail.com>
2024-12-11 09:06:21 +0100

    FIX wp link back to site

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -10,7 +10,7 @@ function Shop() {
             </div>
             <div className="rounded m-2 d-flex flex-xl-row flex-column justify-content-around align-items-center bg-light p-3">
             <img src="/images/entree1jour.png" alt="entree 1 jour" className="img-fluid mx-auto " />
-            <a target="_blank" href="https://nationsoundmetal.rf.gd/wp" ><button className='p-3 m-3 rounded font-weight-bolder bg-info text-dark text-decoration-none h3 text-center lobsterFont'>Acceder à la boutique</button></a> 
+            <a target="_blank" href="https://nationsoundluc.rf.gd/wpdb/boutique/" ><button className='p-3 m-3 rounded font-weight-bolder bg-info text-dark text-decoration-none h3 text-center lobsterFont'>Acceder à la boutique</button></a> 
             <img src="/images/entree3jours.png" alt="entree 1 jour" className="img-fluid mx-auto " />
             </div>
         </div>    
