f7dd12c58afd0542bcdf3ea42cbe84b96ad60456
Crapo01 <capusluc@gmail.com>
2025-06-24 14:54:55 +0200

    removed file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,11 @@ import Actu from "../components/Actu";
 import Concerts from "../components/Concerts";
 import Shop from "../components/Shop";
 import MapMini from "../components/MapMini";
-import Loader from "../components/LocalDatasLoader";
 
 
 function Home() {
     return (
-        <>
-            <Loader/>
+        <>            
             <Concerts />
             <Actu />            
             <Shop/>            
